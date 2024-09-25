3b3f805c848aca271bdce2465a8767925ce87cf0
Шепыч <syndicating@yandex.ru>
2024-09-25 23:24:19 +0300

    Feat: проект сырой, но законченный

--- resources/js/Components/Chat/PersonalChat/PersonalChat.jsx
+++ resources/js/Components/Chat/PersonalChat/PersonalChat.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import styles from './PersonalChat.module.css'
-
+import styles from './PersonalChat.module.css';
 
 export default function PersonalChat({ chatId }) {
     const [messages, setMessages] = useState([]);
@@ -13,21 +12,20 @@ export default function PersonalChat({ chatId }) {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const text = e.target.querySelector('input').value
+        const text = newMessage;
 
         // Отправляем новое сообщение на сервер
         axios.post(`/chats/${chatId}/send_message`, { content: text })
             .then(response => {
                 // Добавляем новое сообщение в список сообщений
-                // setMessages([...messages, response.data]);
                 setNewMessage(''); // Очищаем поле ввода
-                socket.send(JSON.stringify({ type: 'sendMessage', chat_id: chatId, user_id: response.data.user_id, content: text, message_id: response.data.id}));
-                socket.send(JSON.stringify({ type: 'chatUpdate', chat_id: chatId, user_id: response.data.address_id, address_id: userId, content: text}));
+                socket.send(JSON.stringify({ type: 'sendMessage', chat_id: chatId, user_id: response.data.user_id, content: text, message_id: response.data.id }));
+                socket.send(JSON.stringify({ type: 'chatUpdate', chat_id: chatId, user_id: response.data.address_id, address_id: userId, content: text }));
             })
             .catch(error => {
                 console.error('Ошибка при отправке сообщения:', error);
             });
-    }
+    };
 
     useEffect(() => {
         setLoading(true); // Включаем лоадер
@@ -37,7 +35,7 @@ export default function PersonalChat({ chatId }) {
             .then(response => {
                 setMessages(response.data.messages); // Сохраняем сообщения в состояние
                 setLoading(false); // Отключаем лоадер после получения данных
-                setUserId(response.data.user_id)
+                setUserId(response.data.user_id);
             })
             .catch(error => {
                 console.error('Ошибка при загрузке сообщений:', error);
@@ -48,14 +46,14 @@ export default function PersonalChat({ chatId }) {
         const newSocket = new WebSocket('ws://localhost:8888');
 
         // При установлении соединения
-        newSocket.onopen = function() {
+        newSocket.onopen = function () {
             console.log('Соединение установлено');
             // Отправляем данные о присоединении к чату
             newSocket.send(JSON.stringify({ type: 'joinChat', chat_id: chatId }));
         };
 
         // Обрабатываем входящие сообщения
-        newSocket.onmessage = function(event) {
+        newSocket.onmessage = function (event) {
             const message = JSON.parse(event.data);
 
             if (message.type === 'newMessage') {
@@ -87,12 +85,12 @@ export default function PersonalChat({ chatId }) {
         axios.get(`/message/delete/${messageId}`)
             .then(response => {
                 setMessages(prevMessages => prevMessages.filter(message => message.id !== messageId));
-                socket.send(JSON.stringify({ type: 'deleteMessage', chat_id: chatId, message_id: messageId}));
+                socket.send(JSON.stringify({ type: 'deleteMessage', chat_id: chatId, message_id: messageId }));
             })
             .catch(error => {
                 console.error('Ошибка при отправке сообщения:', error);
             });
-    }
+    };
 
     return (
         <div className="flex justify-between w-full flex-col h-full">
@@ -100,26 +98,33 @@ export default function PersonalChat({ chatId }) {
                 {loading ? ( // Показываем лоадер пока данные загружаются
                     <div className="loader"></div>
                 ) : messages.length > 0 ? ( // Если сообщения есть
-                        messages.map((message, index) => (
-                            <div
-                                key={message.id || index} // Используем уникальный идентификатор сообщения или индекс
-                                className={`${styles['message']} ${message.user_id !== userId ? 'self-end' : ''}`}
-                            >
-                                <span className={styles['sender__message']}>{message.user_id}</span>
-                                {message.content}
-                                {(message.user_id !== userId) ? '' : <div className="flex"><span onClick={() => handleMessageDelete(message.id || index)} className={styles['delete__button']}>Удалить</span></div>}
-                            </div>
-                        ))
-                    ) : (
-                        <p>Сообщений пока нет</p> // Если нет сообщений
-                    )}
+                    messages.map((message, index) => (
+                        <div
+                            key={message.id || index} // Используем уникальный идентификатор сообщения или индекс
+                            className={`${styles['message']} ${message.user_id !== userId ? 'self-end' : ''}`}
+                        >
+                            <span className={styles['sender__message']}>{message.user_id}</span>
+                            {message.content}
+                            {(message.user_id !== userId) ? '' : <div className="flex"><span onClick={() => handleMessageDelete(message.id || index)} className={styles['delete__button']}>Удалить</span></div>}
+                        </div>
+                    ))
+                ) : (
+                    <p>Сообщений пока нет</p> // Если нет сообщений
+                )}
             </div>
 
             <div className={styles['footer']}>
                 <form className={`${styles['form__send-message']}`} onSubmit={handleSubmit}>
-                    <input placeholder={'Написать сообщение...'} className={styles['form__input-text']} type="text" name="content"/>
+                    <input
+                        placeholder="Написать сообщение..."
+                        className={styles['form__input-text']}
+                        type="text"
+                        name="content"
+                        value={newMessage} // Управляем значением поля
+                        onChange={(e) => setNewMessage(e.target.value)} // Обрабатываем изменение текста
+                    />
 
-                    <button className='samolet-icon'>
+                    <button className="samolet-icon">
                         <i className="fas fa-paper-plane text-2xl"></i>
                     </button>
                 </form>
